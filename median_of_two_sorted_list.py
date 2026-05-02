4d947f01e81c08011a25cf83325bd016d98da7a8
manilal2357-spec <manilal2357@gmail.com>
2026-05-02 23:29:55 +0530

    finding median in two sorted list

---
+++ median_of_two_sorted_list.py
@@ -0,0 +1,27 @@
+nums1 = [1,2,3,4,5]
+nums2 = [2,3,4,6,8]
+for i in range(len(nums1)):
+    for j in range(len(nums2)):
+        if nums1[i] <= nums2[j]:
+            nums2.insert(j,nums1[i])
+            break
+print(nums2)
+if len(nums2) % 2 == 0:
+    median = ((nums2[len(nums2)//2])+(nums2[(len(nums2)//2)-1]))/2
+    print("median is :- ",median)
+else:
+    median = nums2[len(nums2)//2]
+    print("median is :- ",median)
+
+# another approach
+
+nums1 = [1,2,3,4,5]
+nums2 = [2,3,4,6,8]
+nums2 = sorted(nums1 +nums2)
+print(nums2,"yes")
+if len(nums2) % 2 == 0:
+    median = ((nums2[len(nums2)//2])+(nums2[(len(nums2)//2)-1]))/2
+    print("median is :- ",median)
+else:
+    median = nums2[len(nums2)//2]
+    print("median is :- ",median)
